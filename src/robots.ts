993d532c74da5442ccb87d83a3f7ee19e4add102
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-16 22:18:21 -0400

    create method trimWhitespace

--- src/robots.ts
+++ src/robots.ts
@@ -1,3 +1,8 @@
 export default class RobotsParser {
   constructor() {}
+
+  // remove whitespace before & after line to make parsing each line easier
+  private trimWhitespace(line: string) {
+    return line.trim();
+  }
 }
