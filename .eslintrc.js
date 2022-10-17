3afc761566048ffe6e206c96e6d74a2b9df92a48
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-16 22:16:51 -0400

    update with template file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,32 @@
 module.exports = {
-  env: {
-    node: true,
-    jest: true
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'node', 'prettier'],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json']
   },
-  ignorePatterns: ['.eslintrc.js'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
-      extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'plugin:prettier/recommended'
-      ],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        project: ['./tsconfig.json'], // Specify it only for TypeScript files
-        tsconfigRootDir: __dirname,
-        sourceType: 'module'
-      },
-      plugins: ['@typescript-eslint/eslint-plugin'],
-      rules: {
-        '@typescript-eslint/interface-name-prefix': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-explicit-any': 'off'
-      }
-    },
-    {
-      files: ['*.js'], // Your JavaScript files extension
-      extends: ['plugin:prettier/recommended'],
-      parserOptions: {
-        sourceType: 'module'
-      }
-    },
-    {
-      files: ['/test/**/*.js'], // Your JavaScript files extension
-      extends: ['plugin:prettier/recommended'],
-      parserOptions: {
-        sourceType: 'module'
-      }
-    }
+  extends: [
+    'eslint:recommended',
+    'plugin:node/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended'
   ],
-  root: true
+  rules: {
+    'prettier/prettier': 'warn',
+    'node/no-missing-import': 'off',
+    'node/no-empty-function': 'off',
+    'node/no-unsupported-features/es-syntax': 'off',
+    'node/no-missing-require': 'off',
+    'node/shebang': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    quotes: ['warn', 'single', { avoidEscape: true }],
+    'node/no-unpublished-import': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
 };
