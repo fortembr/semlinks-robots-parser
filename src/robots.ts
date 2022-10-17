130e498c0d46ec1c432b1a5abee953ca1b326506
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-16 22:18:34 -0400

    create method for check for comment

--- src/robots.ts
+++ src/robots.ts
@@ -5,4 +5,14 @@ export default class RobotsParser {
   private trimWhitespace(line: string) {
     return line.trim();
   }
+
+  // flag lines with comments
+  private isComment(line: string) {
+    // the # character starts a comment
+    const commentCharacterFlag = '#';
+    if (line.substring(1) === commentCharacterFlag) {
+      return true;
+    }
+    return false;
+  }
 }
