619d5316628375849a789b22e2663381c571b0c2
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-16 22:27:34 -0400

    create test module details

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export * from './robots';
+import { default as RobotsParser } from './robots';
+
+const robotsParser = new RobotsParser();
+
+module.exports = () => {
+  return robotsParser.readFile('./robots-example.txt');
+};
+
+export const myPackage = (taco = ''): string => `${taco} from my package`;
