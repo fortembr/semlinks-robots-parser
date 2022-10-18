87222c3b1dd3992b7cba392366c0437ec16bced3
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-18 18:28:26 -0400

    move path into function

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export default function robotsParser(filePath: string): void {
   const robotsParser = new RobotsParser();
   robotsParser.readFile('./src/robots-example.txt');
 }
-robotsParser();
+
+robotsParser('./src/robots-example.txt');
