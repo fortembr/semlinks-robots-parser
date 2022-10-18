47b5f2527eb132e0c6190e42b2e73b948d7ba1e1
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-18 00:44:04 -0400

    sanitize and lint file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import { RobotsParser } from './robots'
+import { RobotsParser } from './robots';
 
-export default function robotsParser (): void {
-  const robotsParser = new RobotsParser()
-  robotsParser.readFile('./robots-example.txt')
+export default function robotsParser(): void {
+  const robotsParser = new RobotsParser();
+  robotsParser.readFile('./src/robots-example.txt');
 }
+robotsParser();
