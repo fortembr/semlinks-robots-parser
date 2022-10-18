40da2605bdc2ba219904ce49a12403cf851dfea5
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-18 18:19:17 -0400

    drop types folder from jest

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.spec.ts'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/types/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   globals: {
     'ts-jest': {
       diagnostics: false,
