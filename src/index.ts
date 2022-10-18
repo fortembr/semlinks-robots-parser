408c90d32917fbde0abd1fa709854106c3642498
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-18 18:28:47 -0400

    add param into method

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { RobotsParser } from './robots';
 
 export default function robotsParser(filePath: string): void {
   const robotsParser = new RobotsParser();
-  robotsParser.readFile('./src/robots-example.txt');
+  robotsParser.readFile(filePath);
 }
 
 robotsParser('./src/robots-example.txt');
