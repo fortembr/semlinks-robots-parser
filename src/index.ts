07ef0296c540bb577221c95f749c10148068c13a
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-19 23:28:34 -0400

    add returnType as param

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 import { ReturnType } from './options';
 import { RobotsParser } from './robots';
 
-export default function robotsParser(filePath: string): void {
+export default function robotsParser(returnType: `${ReturnType}`, filePath: string): void {
   const robotsParser = new RobotsParser();
   robotsParser.readFile(filePath);
 }
