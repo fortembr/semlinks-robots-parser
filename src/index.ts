b7c916cc9faf5f361effe4e055f30c5365faabb0
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-20 22:41:51 -0400

    setup function to be async-await

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 import { ReturnType } from './options';
 import { RobotsParser } from './robots';
 
-export default function robotsParser(returnType: `${ReturnType}`, filePath: string) {
+export default async function robotsParser(returnType: `${ReturnType}`, filePath: string) {
   // execute class for variable usage
   const robotsParser = new RobotsParser();
   if (returnType === ReturnType.console) {
@@ -11,6 +11,6 @@ export default function robotsParser(returnType: `${ReturnType}`, filePath: stri
   }
   if (returnType === ReturnType.browser) {
     // format array method
-    return robotsParser.parseFile(filePath);
+    await robotsParser.parseFile(filePath);
   }
 }
