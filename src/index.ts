fdd21a78b174ffd5307071615f3435db9f86586c
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-20 21:55:07 -0400

    move if conditions to index

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,14 @@ import { ReturnType } from './options';
 import { RobotsParser } from './robots';
 
 export default function robotsParser(returnType: `${ReturnType}`, filePath: string) {
+  // execute class for variable usage
   const robotsParser = new RobotsParser();
-  return robotsParser.parseFile(returnType, filePath);
+  if (returnType === ReturnType.console) {
+    // console.log method
+    return robotsParser.logFile(filePath);
+  }
+  if (returnType === ReturnType.browser) {
+    // format array method
+    return robotsParser.parseFile(filePath);
+  }
 }
